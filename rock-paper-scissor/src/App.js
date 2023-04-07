5074d8292fec9019b162def2f94700df48e47d2a
Akriti Srivastava <akisrivastava22@gmail.com>
2023-04-07 17:47:34 +0530

    Computer functionality added

--- rock-paper-scissor/src/App.js
+++ rock-paper-scissor/src/App.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import "./App.css";
 import { FaHandPaper } from "react-icons/fa";
 import { FaFistRaised } from "react-icons/fa";
 import { FaHandPeace } from "react-icons/fa";
 
 function App() {
+  // const actions = {
+  //   rock: FaFistRaised,
+  //   paper: FaHandPaper,
+  //   scissor: FaHandPeace
+
+  // }
   function player(action) {
+
     if (action === "rock") {
       setChoice(<FaFistRaised size={70} />);
     } else if (action === "paper") {
@@ -13,11 +20,44 @@ function App() {
     } else {
       setChoice(<FaHandPeace size={70} />);
     }
+  Computer();
   }
-  // const Computer = () => {};
+
+  function Computer(){
+    const choices = [FaFistRaised, FaHandPaper, FaHandPeace ];
+    setComputechoice(choices[Math.floor(Math.random() * 3)]);
+  };
+
   const [score, setScore] = useState("0");
   const [choice, setChoice] = useState("");
-  const [computchoice, setComputechoice] = useState("");
+  const [computechoice, setComputechoice] = useState("");
+  const [playerWin, setPlayerWin] = useState("");
+
+  const Result = () => {
+    if (choice === FaFistRaised && computechoice === FaHandPeace) {
+      setPlayerWin("win");
+      setScore(score + 1);
+    } else if (choice === FaFistRaised && computechoice === FaHandPaper) {
+      setPlayerWin("lose");
+      setScore(score - 1);
+    } else if (choice === FaHandPeace && computechoice === FaHandPaper) {
+      setPlayerWin("win");
+      setScore(score + 1);
+    } else if (choice === FaHandPeace && computechoice === FaFistRaised) {
+      setPlayerWin("lose");
+      setScore(score - 1);
+    } else if (choice === FaHandPaper && computechoice === FaFistRaised) {
+      setPlayerWin("win");
+      setScore(score + 1);
+    } else if (choice === FaHandPaper && computechoice === FaHandPeace) {
+      setPlayerWin("lose");
+      setScore(score - 1);
+    } else {
+      setPlayerWin("draw");
+    }
+  };
+
+  
   // const [result, setResult] = useState("Player Wins");
   return (
     <div className="first">
@@ -30,7 +70,7 @@ function App() {
           </div>
           <div className="player">
             <div className="play">Computer: {score}</div>
-            <div>{computchoice}</div>
+            <div>{computechoice}</div>
           </div>
         </div>
         <div>
@@ -44,7 +84,12 @@ function App() {
             <FaHandPeace />
           </button>
         </div>
-        <h1>Player Wins</h1>
+
+        <h1>Result:
+        {playerWin == "win" && <h2>You Win</h2>}
+        {playerWin == "lose" && <h2>You lose</h2>}
+        {playerWin == "draw" && <h2>Draw</h2>}
+        </h1>
       </div>
     </div>
   );
