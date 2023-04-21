dbf91930c4136cec635692666f0e875cef95ec59
Akriti Srivastava <akisrivastava22@gmail.com>
2023-04-21 22:35:35 +0530

    JS functionality completed

--- pomodoro-app/src/Tags.js
+++ pomodoro-app/src/Tags.js
@@ -2,34 +2,99 @@ import { useState } from "react";
 import React from "react";
 
 const Tags = () => {
-    const [timer , setTimer] = useState("00:00");
-  function  handlework (){
-    setTimer("25:00")
-    let r= 24;
-    let sec = 60 ;
-    setInterval(() => {
-        sec--;
-        if (sec === -1) 
-        {
-         r--;
-         sec = 59;    
-        }
-        setTimer(r + ":" + sec)
-
-    }, 1000)
-
-
+  const [myInterval, setMyInterval] = useState(0); 
+  const [timer, setTimer] = useState();
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const [isPause, setIsPause] = useState(false);
+  function handleTimer(seconds) {
+    setTimer(seconds);
+    setIsTimerRunning(true);
+    if (timer === 0) {
+        console.log("Prateeeeeeeeeeeeek");
+        clearInterval(myInterval);
+        return ;
+    }
+   let newInterval = setInterval(() => {
+        seconds--;
+        setTimer(seconds);
+      }, 1000);
+      setMyInterval(newInterval);
   }
-    return (
-        <div>
-        <div className="text-white text-center">
-            {timer}
-        </div>
-        <button onClick={() => {handlework()}} className="bg-white rounded-lg px-10">Work</button>
-        <button className="bg-white ml-2 rounded-lg px-10 ">Short Break</button>
-        <button className="bg-white ml-2 rounded-lg px-10">Long Break</button>
-        </div>
-    )
-}
+  function time() {
+    let min = parseInt(timer / 60);
+    let sec = timer % 60;
+    let temp = "";
+    if (min < 10) {
+      temp = "0" + min;
+    } else {
+      temp = temp + min;
+    }
+    temp += ":";
+    if (sec < 10) {
+      temp += "0" + sec;
+    } else {
+      temp = temp + sec;
+    }
+    if (isNaN(min)){
+        temp= "00:00";
+    }
+    return temp;
+  }
+
+    function handleReset(){
+        setIsTimerRunning(false);
+        clearInterval(myInterval);
+        setTimer(null);
+    }
+
+    function handlePause(){
+        clearInterval(myInterval);
+        setIsPause(true);
+        if (isPause) {
+            handleTimer(timer);
+            setIsPause(false);
+        }
+    }
+  return (
+    <div>
+      <div className="text-white text-center">
+        {time()}
+      </div>
+      <div className="text-center mt-3">
+      { isTimerRunning ? 
+        (<>
+    <button className="bg-white rounded-lg px-10" onClick={handlePause}>{isPause ? "Start" : "Pause"}</button>
+    <button className="bg-white rounded-lg px-10 ml-3" onClick={handleReset}>Rest</button>
+    </>) :
+       ( <> 
+    <button
+          onClick={() => {
+            handleTimer(1500);
+          }}
+          className="bg-white rounded-lg px-10"
+        >
+          Work
+        </button>
+        <button
+          onClick={() => {
+            handleTimer(300);
+          }}
+          className="bg-white ml-2 rounded-lg px-10 "
+        >
+          Short Break
+        </button>
+        <button
+          onClick={() => {
+            handleTimer(900);
+          }}
+          className="bg-white ml-2 rounded-lg px-10"
+        >
+          Long Break
+        </button>
+  </>)}
+    </div>
+    </div>
+  );
+};
 
 export default Tags;
